51100e9e5606b2c2e73fafb89f572f0462f58444
Mallikarjuna29429 <kambala.mallikarjuna@gmail.com>
2023-01-06 19:28:13 +0530

    header

---
+++ src/components/header.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./header.css";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+import SearchIcon from "@material-ui/icons/Search";
+import { Link } from "react-router-dom";
+import { ShoppingCart } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+// import { useStateValue } from "./StateProvider";
+
+function Header() {
+  const navigate = useNavigate();
+  const cart = useSelector((state) => state.cart);
+
+  const getTotalQuantity = () => {
+    let total = 0;
+    cart.forEach((item) => {
+      console.log(item);
+      total += item.quantity;
+    });
+    console.log(total);
+    return total;
+  };
+  // const [{ basket }, dispatch] = useStateValue();
+  return (
+    <div className="header">
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <div className="header__logo">
+          <StorefrontIcon className="header__logoImage" fontSize="large" />
+          <h2 className="header__logoTitle">eShop</h2>
+        </div>
+      </Link>
+
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header__nav">
+        <Link to="/login" style={{ textDecoration: "none" }}>
+          <div className="nav__item">
+            <span className="nav__itemLineOne">Hello Guest</span>
+            <span className="nav__itemLineTwo">Sign In</span>
+          </div>
+        </Link>
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Your</span>
+          <span className="nav__itemLineTwo">Shop</span>
+        </div>
+        <Link to="/checkout" style={{ textDecoration: "none" }}>
+          {/* <div className="nav__itemBasket">
+            <ShoppingBasketIcon />
+            <span className="nav__itemLineTwo nav__basketCount">
+              
+            </span>
+          </div> */}
+        </Link>
+        <div className="shopping-cart" onClick={() => navigate("/cart")}>
+          <ShoppingCart id="cartIcon" />
+          <p>{getTotalQuantity() || 0}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
