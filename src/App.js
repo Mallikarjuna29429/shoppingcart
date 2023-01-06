44c3158d2dac6be0ec859b538bd3cdfcd4644288
Mallikarjuna29429 <kambala.mallikarjuna@gmail.com>
2023-01-06 19:29:21 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
-import Cart from './pages/Cart';
-import Home from './pages/Home';
-import {Routes, Route} from 'react-router-dom'
-
+import "./App.css";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header";
 function App() {
   return (
     <div className="app">
+      <Header />
       <Routes>
-        <Route exact path='/' element={<Home />}/>
-        <Route path='/cart' element={<Cart />}/>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </div>
   );
