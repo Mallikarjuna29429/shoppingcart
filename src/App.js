f60b21286e28d1e096e4f2a29f1d8e661257bb39
Mallikarjuna29429 <kambala.mallikarjuna@gmail.com>
2023-01-06 22:57:16 +0530

    search

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/header";
+import React, { Component, useState, createContext } from "react";
+const SearchContext = createContext();
+
 function App() {
+  const [search, setSearch] = useState("");
   return (
-    <div className="app">
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-    </div>
+    <SearchContext.Provider value={search}>
+      <div className="app">
+        <Header setSearch={setSearch} />
+        <Routes>
+          <Route exact path="/" element={<Home search={search} />} />
+          <Route path="/cart" element={<Cart search={search} />} />
+        </Routes>
+      </div>
+    </SearchContext.Provider>
   );
 }
 
